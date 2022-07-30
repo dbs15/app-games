f6ecb33a5d689d74d37fdda45f86d66eefee927d
dbs15 <baenaneo@gmail.com>
2022-07-30 18:41:24 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by David on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
