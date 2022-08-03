4c949069246038147be57c4a3373b48ba6895c11
dbs15 <baenaneo@gmail.com>
2022-08-02 21:59:32 -0500

    logica pantalla 1

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -13,24 +13,73 @@ struct ContentView: View {
             Color(red: 17/255, green: 28/255, blue: 52/255, opacity: 1.0).ignoresSafeArea()
             
             VStack{
-                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250)
-                HomeAndResgister()
+                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 42)
+                
+                HomeAndResgisterView()
                 
             }
         }
     }
 }
 
-struct HomeAndResgister: View {
+struct HomeAndResgisterView: View {
+    
+    @State var loginType = true
+    
     var body: some View{
         
         VStack {
             
             HStack {
-                Text("INICIA SESIÓN")
-                Text("REGISTRATE")
-            }.foregroundColor(.white)
+                
+                Spacer()
+                
+                Button("INICIA SESIÓN") {
+                    loginType = true
+                    print("inicio de sesion")
+                }
+                .foregroundColor(loginType ? .white : .gray)
+                
+                Spacer()
+                
+                Button("REGISTRATE") {
+                    loginType = false
+                    print("REGISTRATE")
+                }
+                .foregroundColor(loginType ? .gray : .white)
+                
+                Spacer()
+                
+                
+            }
+            
+            Spacer(minLength: 42)
+            
+            if loginType == true {
+                LoginView()
+            }else{
+                RegisterView()
+            }
         }
+    }
+}
+
+struct LoginView:View {
+    
+    
+    var body: some View{
+        
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Correo Electrónico").foregroundColor(Color("Dark-Cian"))
+            }.padding(.horizontal, 77.0)
+        }
+    }
+}
+
+struct RegisterView:View {
+    var body: some View{
+        Text("soy la vista de registro")
     }
 }
 
